4b5e7d4ef871bebb3c7d467943fd042f41218750
Tisha <fatimajannat20188@gmail.com>
2021-02-15 23:45:50 -0800

    first commit

---
+++ firstLaravel/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route :: get('/name',function(){
+
+    return "<h1>Fatima Jannat Tisha</h1>";
+});
+
+Route::get('/myintro', function () {
+    return view('myintro');
+});
